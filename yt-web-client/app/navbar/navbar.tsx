884a419c42f2070b3e8c15795debc83fa2d97e3b
Yskak <shiyap.yskak@gmail.com>
2023-12-22 00:50:49 +0500

    Add navbar module and update package.json and layout.tsx

---
+++ yt-web-client/app/navbar/navbar.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import SignIn from "./sign-in";
+import Link from "next/link";
+
+import styles from "./navbar.module.css";
+// import Upload from "./upload";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
+
+
+function NavBar() {
+  // Initialize user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [] /* No dependencies, never rerun */);
+
+
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <span className={styles.logoContainer}>
+          <img className={styles.logo} src="/youtube-logo.svg" alt="YouTube Logo" />
+        </span>
+      </Link>
+      <SignIn user={user} />
+    </nav>
+  );
+}
+
+export default NavBar;
